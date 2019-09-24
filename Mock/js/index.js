6a6147bda941f4a76edd8a92fa1c5e002e72f6c0
16110035_Lê Văn Duy <47024529+duyle0207@users.noreply.github.com>
2019-09-24 15:36:30 +0700

    content & detail

---
+++ Mock/js/index.js
@@ -0,0 +1,11 @@
+function ChangeImage(image)
+{
+    var source = image.src;
+    document.getElementById("main_img").src = source;
+}
+
+function SetDetailImage(image)
+{
+    var source = image.src;
+    document.getElementById("detail-img").src = source;
+}
