30e81ea5b18bba87c5bbcd65d5e7cc9faedc49b9
16110035_Lê Văn Duy <47024529+duyle0207@users.noreply.github.com>
2019-11-20 10:13:21 +0700

    Fix login

--- ReactJS/my-app/src/components/admin/login/login.jsx
+++ ReactJS/my-app/src/components/admin/login/login.jsx
@@ -48,6 +48,7 @@ class login extends Component {
             // console.log(data !== undefined);
             if (data !== undefined) {
                 if (data.authorities[0].authority === "Admin") {
+                    alert("Success");
                     localStorage.setItem("adminInfo", JSON.stringify(data))
                     this.setState({ userInfo: JSON.parse(localStorage.getItem("adminInfo")) });
                     console.log(this.state.userInfo);
